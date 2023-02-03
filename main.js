ddc2041986b3b6449d26ccfd38a5d3ad9dd97192
huydayne168 <huynguyenviet1608@gmail.com>
2023-02-03 10:19:12 +0700

    done

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-// ======================auto slide=======================
+// ======================blog slider=======================
+
+window.addEventListener("load", function () {
+    const blogSlider = document.querySelector(".blog-slider");
+    const blogSliderWrapper = document.querySelector(".blog-slider--wrapper");
+    const blogSliderMain = document.querySelector(".cards");
+    const blogSliderItems = document.querySelectorAll(".card-item");
+    const prevBtn = document.querySelector(".prev-btn img");
+    const nextBtn = document.querySelector(".next-btn img");
+
+    const sliderItemWidth = blogSliderItems[0].offsetWidth;
+    const slideLength = blogSliderItems.length;
+
+    let positionX = 0;
+    let index = 0;
+
+    prevBtn.addEventListener("click", function () {
+        handleChangSlide(-1);
+    });
+    nextBtn.addEventListener("click", function () {
+        handleChangSlide(1);
+    });
+
+    function handleChangSlide(dir) {
+        if (dir == 1) {
+            if (index >= 0 && index < slideLength - 3) {
+                positionX = positionX - sliderItemWidth - 30;
+                blogSliderMain.style = `transform: translate(${positionX}px)`;
+                index++;
+                console.log(index);
+            }
+        } else if (dir == -1) {
+            if (index > 0 && index <= slideLength - 3) {
+                positionX = positionX + sliderItemWidth + 30;
+                blogSliderMain.style = `transform: translate(${positionX}px)`;
+                index--;
+                console.log(index);
+            }
+        }
+    }
+});
